870c20bbb7c2482cc7b557e014a486603f2f7ad9
Anton <superantoxa12@gmail.com>
2022-11-07 01:09:59 +0100

    Initial commit

---
+++ ToDoList/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ToDoList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ToDo todo = new ToDo();
+
+            todo.Start();
+        }
+    }
+}
---
+++ ToDoList/Task.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+    class Task
+    {
+        public int id;
+        private string taskText;
+        public Task(int id, string taskText)
+        {
+            this.id = id;
+            this.taskText = taskText;
+        }
+        //public void SetId(int id)
+        //{
+        //    this.id = id;
+        //}
+        public int GetId()
+        {
+            return id;
+        }
+        public string GetTaskText()
+        {
+            return taskText;
+        }
+
+    }
+}
